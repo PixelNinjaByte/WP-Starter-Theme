c59bec3c9554ad7925af634c1a0720755e2eb2fb
Yuvaraj <179440168+kyuvarajog@users.noreply.github.com.>
2025-11-23 00:04:04 +0530

    modified enqueue

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,8 +1,22 @@
 <?php
 
 function wpstarter_enqueue_scripts() {
-    wp_enqueue_style('wpstarter-style', get_stylesheet_uri());
 
+    // Load root style.css (required for theme)
+    wp_enqueue_style(
+        'wpstarter-style',
+        get_stylesheet_uri()
+    );
+
+    // Load custom CSS in assets/css/style.css
+    wp_enqueue_style(
+        'wpstarter-custom',
+        get_template_directory_uri() . '/assets/css/style.css',
+        array('wpstarter-style'),
+        false
+    );
+
+    // Load JS
     wp_enqueue_script(
         'wpstarter-main',
         get_template_directory_uri() . '/assets/js/main.js',
